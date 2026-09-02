524bc56c033ae18d7e6d0fefc6f432c045bb0717
DanielBerns <daniel.w.berns@gmail.com>
2026-09-02 17:53:37 -0300

    test run_news_grab.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-# Writes current ISO 8601 UTC datetime to timestamp.txt
-date -u +"%Y-%m-%dT%H:%M:%SZ" > timestamp.txt
-echo "Updated timestamp.txt with current ISO datetime: $(cat timestamp.txt)"
+# Writes current ISO 8601 UTC datetime to start.txt
+date -u +"%Y-%m-%dT%H:%M:%SZ" > start.txt
+echo "Updated start.txt with current ISO datetime: $(cat start.txt)"
+cd ~/Commands
+./run_news_grab.sh
+cd -
+date -u +"%Y-%m-%dT%H:%M:%SZ" > stop.txt
+echo "Updated stop.txt with current ISO datetime: $(cat stop.txt)"
