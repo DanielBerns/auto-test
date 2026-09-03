1c9bfa725fde9ef887b84cf027e68ea9b6c341a0
DanielBerns <daniel.w.berns@gmail.com>
2026-09-03 19:55:53 -0300

    news grab again

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 date -u +"%Y-%m-%dT%H:%M:%SZ" > start.txt
 echo "Updated start.txt with current ISO datetime: $(cat start.txt)"
 cd ~/Commands
-./run_weather_forecast.sh
+./run_news_grab.sh
 cd -
 date -u +"%Y-%m-%dT%H:%M:%SZ" > stop.txt
 echo "Updated stop.txt with current ISO datetime: $(cat stop.txt)"
