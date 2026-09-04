80c05256c5560bca9a7662ee47eab0b0276fb8d4
DanielBerns <daniel.w.berns@gmail.com>
2026-09-03 22:41:18 -0300

    test with ls

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 date -u +"%Y-%m-%dT%H:%M:%SZ" > start.txt
 echo "Updated start.txt with current ISO datetime: $(cat start.txt)"
 cd ~/Commands
-./run_news_grab.sh
+ls -a
 cd -
 date -u +"%Y-%m-%dT%H:%M:%SZ" > stop.txt
 echo "Updated stop.txt with current ISO datetime: $(cat stop.txt)"
